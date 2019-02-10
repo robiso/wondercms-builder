1f243dd380b874b0072c508170ab39e9b0d54283
Nicolas CARPi <nicolas.carpi@curie.fr>
2019-02-10 23:44:56 +0100

    Add build.sh and LICENSE

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # wondercms-builder
 
+## Description
+
 Build a zip archive for WonderCMS so it can work without any build step.
+
+## Requirements
+
+Needs to have `git`, `yarn`, `zip` and `composer` in `$PATH`.
+
+## Usage
+
+~~~bash
+./build.sh
+~~~
---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Build a zip archive with wondercms files
+
+# change this to master once the dev branch has been merged
+BRANCH=dev
+# change this to the proper location of the zip file
+ZIP_PATH=/tmp/latest.zip
+
+TMP_DIR=/tmp/wondercms-$RANDOM
+
+# get the latest files
+git clone --depth 1 -b dev https://github.com/robiso/wondercms "$TMP_DIR"
+
+cd "$TMP_DIR"
+# install javascript dependencies
+yarn install
+# minify JS and CSS
+grunt
+# install php dependencies
+composer install
+
+# make zip archive
+# but first remove the previous one (or it will just append to it)
+if [ -f "$ZIP_PATH" ]; then
+    rm "$ZIP_PATH"
+fi
+
+zip -r "$ZIP_PATH" assets index.php plugins src/classes themes vendor version
