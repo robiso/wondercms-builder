2d6e2682d96cd3e901e9fefd15f1b71da329df32
notcarbide <41094677+notcarbide@users.noreply.github.com>
2019-03-25 15:11:38 -0500

    Build everything internally & fix apache builds (#2)
    * sh: build: Update to build within source dir & for apache
    
    - Build everything internally by default.
    
    - Add an option to include htaccess files for apache servers. (if
      not, created wondercms zip will not work by default on apache)
    
    * Add gitignore
    
    - Ignore default outputs from build script
    
    * Lint for POSIX standard
    
    - Using the latest from:
        https://github.com/koalaman/shellcheck
    
    - Fix typo in comment
    
    * Improvements based on comments
    
    -  See @NicolasCARPi comments on #2

--- build.sh
+++ build.sh
@@ -3,15 +3,31 @@
 
 # change this to master once the dev branch has been merged
 BRANCH=dev
-# change this to the proper location of the zip file
-ZIP_PATH=/tmp/latest.zip
+# change this to where the script should output the zip file
+ZIP_PATH=$(pwd)/out
 
-TMP_DIR=/tmp/wondercms-$RANDOM
+# attempt to make zip path if does not exist
+if [ ! -d "$ZIP_PATH" ]; then
+    mkdir -p "$ZIP_PATH"
+fi
+
+# name zip file based on branch and date
+CDATE=$(date "+%Y-%m-%d")
+ZIP_FILE=$ZIP_PATH/wondercms_$BRANCH-$CDATE.zip
+
+# change this to set where WonderCMS should be downloaded and built
+BUILD_DIR=src/wondercms_$BRANCH-$CDATE
+
+# attempt to make directory for building if does not exist
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir -p "$BUILD_DIR"
+fi
 
 # get the latest files
-git clone --depth 1 -b "$BRANCH" https://github.com/robiso/wondercms "$TMP_DIR"
+git clone --depth 1 -b "$BRANCH" https://github.com/robiso/wondercms "$BUILD_DIR"
 
-cd "$TMP_DIR"
+# change to git clone directory
+cd "$BUILD_DIR" || exit
 # install javascript dependencies
 yarn install
 # minify JS and CSS
@@ -19,10 +35,41 @@ yarn install
 # install php dependencies
 composer install
 
-# make zip archive
-# but first remove the previous one (or it will just append to it)
-if [ -f "$ZIP_PATH" ]; then
-    rm "$ZIP_PATH"
+# remove the previous zip (or it will just append to it)
+if [ -f "$ZIP_FILE" ]; then
+    rm "$ZIP_FILE"
 fi
 
-zip -r "$ZIP_PATH" assets index.php plugins src/classes themes vendor version
+# check for apache command to include htaccess files
+if [ "$1" == "--apache" ]; then
+    ZIP_APPEND=$(echo .htaccess*)
+else
+    ZIP_APPEND=""
+fi
+
+# make zip archive
+zip -r "$ZIP_FILE" assets index.php plugins src/classes themes vendor version $ZIP_APPEND
+
+# list extra apache files included
+# or notify that invalid command was ignored
+echo ""
+if [ -n "$ZIP_APPEND" ]; then
+    echo "Option $1 was selected. Additional files included:"
+    for arg in $ZIP_APPEND; do
+        echo "    $arg"
+    done
+    echo ""
+elif [ -n "$1" ]; then
+    echo "Unknown option $1 was ignored!"
+    echo "Use '.build.sh --apache' to build for an apache server."
+    echo ""
+fi
+
+# check for successful zip creation
+if [ -f "$ZIP_FILE" ]; then
+    echo "Build in $BUILD_DIR successful!"
+    echo "Output: $ZIP_FILE"
+else
+    echo "Build command failed!"
+    exit 1
+fi
